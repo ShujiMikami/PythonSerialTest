f17ed2275f0ebf9eb2b94da1e6ee5bd4f4c55cf3
Shuji Mikami <carbonized.pan@gmail.com>
2018-08-26 17:01:20 +0900

    テストクラス作成

--- testmodule.py
+++ testmodule.py
@@ -9,6 +9,43 @@ import serial
 from pip._vendor.distlib.compat import raw_input
 from time import sleep
 
+class Test:
+    __serialPort = 0
+    
+    def Connect(self):
+        self.__serialPort = serial.Serial(port="/dev/tty.usbserial-AH01KQHK", baudrate=115200, timeout=5, parity=serial.PARITY_NONE)
+        self.__serialPort.reset_input_buffer()
+    
+        if self.__serialPort.out_waiting > 0:
+            self.__serialPort.reset_output_buffer()
+    
+    def __waitForText(self, waitString, timeOut):
+        flag = False
+        cnt = 0
+        while flag == False:
+            if self.__serialPort.in_waiting > 0:
+                responseText = self.__serialPort.readline()
+                print(responseText)
+                
+                if responseText == waitString.encode():
+                    flag = True
+            
+            sleep(0.1)
+            cnt = cnt + 1
+            
+            if cnt > timeOut:
+                print("timeout")
+                flag = True
+    
+    def __writeText(self, sendMessage):
+        self.__serialPort.write(sendMessage.encode())
+    
+    def SendMessage(self, sendMessage, waitMessage, replyTimeOut):
+        self.__writeText(sendMessage)
+        self.__waitForText(waitMessage, replyTimeOut)
+    
+    def Disconnect(self):
+        self.__serialPort.close()
 def TxTest():
     processStatus = True
     
